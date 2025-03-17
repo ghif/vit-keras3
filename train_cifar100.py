17ccac7e6cc31b0c9a550c77a6fc32635299c1fc
Muhammad Ghifary <mghifary@192.168.1.6>
2025-03-17 19:20:34 +0700

    no mixed precision

--- train_cifar100.py
+++ train_cifar100.py
@@ -29,8 +29,8 @@ input_shape = (32, 32, 3)
 train_dataset, _ = get_dataset(conf.BATCH_SIZE, is_training=True)
 test_dataset, _ = get_dataset(conf.BATCH_SIZE, is_training=False)
 
-# Use mixed precision
-keras.mixed_precision.set_global_policy("mixed_float16")
+# # Use mixed precision
+# keras.mixed_precision.set_global_policy("mixed_float16")
 
 vit_model = M.vit_classifier(
     orig_image_shape=input_shape,
