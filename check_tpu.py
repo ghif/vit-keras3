071646a834b15402cc0cc006e362a3da256d1ccf
Muhammad Ghifary <mghifary@192.168.1.7>
2025-04-20 17:52:54 +0700

    check tpu

--- check_tpu.py
+++ check_tpu.py
@@ -5,5 +5,10 @@ os.environ["KERAS_BACKEND"] = "jax"
 import keras
 import jax
 
-devices = jax.devices("gpu")
+devices = jax.devices("tpu")
 print(f"devices: {devices}")
+
+# Define a 2x4 device mesh with data and model parallel axes
+mesh = keras.distribution.DeviceMesh(
+    shape=(2, 4), axis_name=["data", "model"], devices=devices
+)
