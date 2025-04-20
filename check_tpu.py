ceb84db941b30a5ce913fb37e60abfde496cddd8
Muhammad Ghifary <mghifary@192.168.1.7>
2025-04-20 17:56:52 +0700

    tpu layout

--- check_tpu.py
+++ check_tpu.py
@@ -10,5 +10,13 @@ print(f"devices: {devices}")
 
 # Define a 2x4 device mesh with data and model parallel axes
 mesh = keras.distribution.DeviceMesh(
-    shape=(2, 4), axis_name=["data", "model"], devices=devices
+    shape=(2, 4), axis_names=["data", "model"], devices=devices
+)
+
+# A 2D layout, which describes how a tensor is distributed across the mesh
+layout_2d = keras.distribution.TensorLayout(axes=("model", "data"), device_mesh=mesh)
+
+# A 4D layout, which could be used for data parallel of an image input
+replicated_layout_4d = keras.distribution.TensorLayout(
+    axes=("data", None, None, None), device_mesh=mesh
 )
