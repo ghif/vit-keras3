acfe49ca0534eb80d342cd11eadc5581d205e88e
Muhammad Ghifary <mghifary@192.168.1.7>
2025-04-21 14:32:09 +0700

    debug check tpu tf

--- check_tpu_tf.py
+++ check_tpu_tf.py
@@ -6,6 +6,6 @@ resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu="local")
 # You can then proceed with initialization
 tf.config.experimental_connect_to_cluster(resolver)
 tf.tpu.experimental.initialize_tpu_system(resolver)
-strategy = tf.distribute.TPUStrategy(resolver)
+# strategy = tf.distribute.TPUStrategy(resolver)
 
 print("All devices: ", tf.config.list_logical_devices('TPU'))
