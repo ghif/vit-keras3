06109ee84cdad1c1dca253fbf89bd44eb507328f
Muhammad Ghifary <mghifary@192.168.1.7>
2025-04-21 14:41:46 +0700

    debug check tpu tf

--- check_tpu_tf.py
+++ check_tpu_tf.py
@@ -1,11 +1,24 @@
 import tensorflow as tf
 
+try:
+    # Inside the VM, 'local' or no argument often works
+    # resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
+    resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu="local")
+
+    tf.config.experimental_connect_to_cluster(resolver)
+    tf.tpu.experimental.initialize_tpu_system(resolver)
+    strategy = tf.distribute.TPUStrategy(resolver)
+    print("Successfully initialized TPU.")
+    print("All devices: ", tf.config.list_logical_devices('TPU'))
+except Exception as e:
+    print(f"Failed to initialize TPU: {e}")
+
 # Provide the TPU name explicitly
-resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu="local")
+# resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu="local")
 
-# You can then proceed with initialization
-tf.config.experimental_connect_to_cluster(resolver)
-tf.tpu.experimental.initialize_tpu_system(resolver)
-# strategy = tf.distribute.TPUStrategy(resolver)
+# # You can then proceed with initialization
+# tf.config.experimental_connect_to_cluster(resolver)
+# tf.tpu.experimental.initialize_tpu_system(resolver)
+# # strategy = tf.distribute.TPUStrategy(resolver)
 
-print("All devices: ", tf.config.list_logical_devices('TPU'))
+# print("All devices: ", tf.config.list_logical_devices('TPU'))
